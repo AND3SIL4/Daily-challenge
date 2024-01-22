b40d3e9ac75943a932a46fad8ebfd3e2fd3d8e6e
AND3SIL4 <afsilva6236@misena.edu.co>
2024-01-22 17:43:20 -0500

    UDATE: exercises to test some code

--- javascript/_otro.js
+++ javascript/_otro.js
@@ -1,7 +1,22 @@
-function print (value){
-  for (let i = 0; i < value; i++) {
-    console.log('* '.repeat(value));
+const tickets = {
+  '0H2AZ123': null,
+  '23LA9T41': 'Verena Nardi',
+  'something': {
+    name: 'andres'
   }
+};
+
+const tickets2 = {
+  '0H2AZ123': null,
+  '23LA9T41': 'Verena Nardi',
+};
+
+
+function main(tickets, ticketId) {
+  return tickets.something?.name
 }
 
-print(3);
+
+console.log(main(tickets, 'RE90VAW7'));
+console.log(main(tickets2, '0H2AZ123'));
+console.log(main(tickets, '23LA9T41'));
